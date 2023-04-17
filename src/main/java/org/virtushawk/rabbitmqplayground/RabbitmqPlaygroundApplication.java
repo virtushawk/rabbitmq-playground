7d1ac1ce898aa1e01175347841b6b1b942cbb9d6
Roman <roma.bruhanchik@gmail.com>
2023-04-17 20:12:33 +0400

    Initial commit

---
+++ src/main/java/org/virtushawk/rabbitmqplayground/RabbitmqPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package org.virtushawk.rabbitmqplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/virtushawk/rabbitmqplayground/RabbitmqPlaygroundApplicationTests.java
@@ -0,0 +1,21 @@
+package org.virtushawk.rabbitmqplayground;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class RabbitmqPlaygroundApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertNotNull(applicationContext);
+    }
+
+}
