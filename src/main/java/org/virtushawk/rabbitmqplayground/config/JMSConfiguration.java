ecc5c8b2627726c8ca46031106a0404fe39400f9
Roman <roma.bruhanchik@gmail.com>
2023-04-20 23:26:35 +0400

    Add wait queue for failed messages

--- src/main/java/org/virtushawk/rabbitmqplayground/config/JMSConfiguration.java
+++ src/main/java/org/virtushawk/rabbitmqplayground/config/JMSConfiguration.java
@@ -1,19 +1,15 @@
 package org.virtushawk.rabbitmqplayground.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.retry.RepublishMessageRecoverer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.interceptor.RetryOperationsInterceptor;
+import org.virtushawk.rabbitmqplayground.jms.RepublishMessageMaxAttempts;
 
 @Configuration
 public class JMSConfiguration {
@@ -22,17 +18,23 @@ public class JMSConfiguration {
 
     public static final String DEAD_LETTER_EXCHANGE = "dead-letter-exchange";
 
+    public static final String WAIT_LETTER_EXCHANGE = "wait-letter-exchange";
+
     public static final String PROCESS_ORDER_QUEUE = "q.process-order";
 
     public static final String RETRY_ORDER_QUEUE = "q.retry-process-order";
 
     public static final String FALL_BACK_ORDER_QUEUE = "q.fall-back-process-order";
 
+    public static final String WAIT_PROCESS_ORDER_QUEUE = "q.wait-process-order";
+
     public static final String PROCESS_ORDER_ROUTING_KEY = "process.order";
 
     public static final String FALL_BACK_ROUTING_KEY = "fall-back";
 
     public static final String RETRY_PROCESS_ORDER_ROUTING_KEY = "retry.process.order";
+
+    public static final String WAIT_PROCESS_ORDER_ROUTING_KEY = "wait.process.order";
 
     private final CachingConnectionFactory cachingConnectionFactory;
 
@@ -56,6 +58,15 @@ public class JMSConfiguration {
     }
 
     @Bean
+    Queue waitQueue() {
+        return QueueBuilder.durable(WAIT_PROCESS_ORDER_QUEUE)
+                .deadLetterExchange(SIMPLE_EXCHANGE)
+                .deadLetterRoutingKey(PROCESS_ORDER_ROUTING_KEY)
+                .ttl(1000)
+                .build();
+    }
+
+    @Bean
     Binding simpleQueueBinding() {
         return BindingBuilder.bind(queue()).to(topicExchange()).with(PROCESS_ORDER_ROUTING_KEY);
     }
@@ -71,6 +82,11 @@ public class JMSConfiguration {
     }
 
     @Bean
+    Binding waitQueueBinding() {
+        return BindingBuilder.bind(waitQueue()).to(waitExchange()).with(WAIT_PROCESS_ORDER_ROUTING_KEY);
+    }
+
+    @Bean
     public TopicExchange topicExchange() {
         return new TopicExchange(SIMPLE_EXCHANGE);
     }
@@ -78,6 +94,11 @@ public class JMSConfiguration {
     @Bean
     public DirectExchange directExchange() {
         return new DirectExchange(DEAD_LETTER_EXCHANGE);
+    }
+
+    @Bean
+    public DirectExchange waitExchange() {
+        return new DirectExchange(WAIT_LETTER_EXCHANGE);
     }
 
     @Bean
@@ -89,10 +110,10 @@ public class JMSConfiguration {
 
     @Bean
     public RetryOperationsInterceptor retryInterceptor(){
-        return RetryInterceptorBuilder.stateless().maxAttempts(3)
-                .backOffOptions(2000, 2.0, 100000)
-                .recoverer(new RepublishMessageRecoverer(rabbitTemplate(), DEAD_LETTER_EXCHANGE,
-                        FALL_BACK_ROUTING_KEY))
+        return RetryInterceptorBuilder.stateless()
+                .maxAttempts(1)
+                .recoverer(new RepublishMessageMaxAttempts(rabbitTemplate(), WAIT_LETTER_EXCHANGE,
+                        WAIT_PROCESS_ORDER_ROUTING_KEY, DEAD_LETTER_EXCHANGE, FALL_BACK_ROUTING_KEY, 2))
                 .build();
     }
 
